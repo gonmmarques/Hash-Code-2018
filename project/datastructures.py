42ee82e5a1305adb720907cb4221b643b77dcfe9
Guilherme Ramos <ramosguilhermefilipe@gmail.com>
2018-03-01 19:20:54 +0000

    Commit

--- project/datastructures.py
+++ project/datastructures.py
@@ -11,13 +11,31 @@ class Ride():
         self.f = f
 
 
+    def calculateDistance(self):
+        return calculateGenericDistance(self.a,self.b,self.x,self.y)
+
+    def calculateTime(self, car):
+        return car.
+
+
 
 class Car():
 
     def __init__(self, id, xInit, yInit):
         super(Car, self).__init__()
-        self.id = id
+        # self.id = id
         self.x = xInit
         self.y = yInit
         self.steps = 0
         self.rideList = []
+
+    def addRide(self, ride):
+        self.steps += ride.s
+        self.rideList.append(ride)
+
+    def calculatePreDistance(self, ride):
+        return calculateGenericDistance(self.x, self.y, ride.a, ride.b)
+
+
+def calculateGenericDistance(xi, yi, xf, yf):
+    return abs(xf - xi) + abs(yf - yi)
